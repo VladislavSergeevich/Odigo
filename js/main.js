b0ab93eb0f342c06913d236dbe3ac0a1e9126a87
VladislavSergeevich <60568407+VladislavSergeevich@users.noreply.github.com>
2020-02-09 15:17:03 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 (function () {
     const header = document.querySelector('.header');
     window.onscroll = () => {
-        if (window.pageYOffset >50){
+        if (window.pageYOffset > 50) {
             header.classList.add('header-active');
+        } else {
+            header.classList.remove('header-active');
         }
-        else
-            {
-                header.classList.remove('header-active');
-            }
     }
+}());
+
+// Burger handler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuClose = document.querySelector('.header-nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
 }());
